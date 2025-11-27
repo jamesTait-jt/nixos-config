8acf5212a4a0773745476d7675bd8fe43587046b
James Tait <james.tait@fonoa.com>
2025-11-27 14:58:24 +0000

    Make git signs better

--- home/nvim/nvim/lua/jamestait/lazy/gitsigns.lua
+++ home/nvim/nvim/lua/jamestait/lazy/gitsigns.lua
@@ -1,21 +1,23 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-		signs = {
-			add = { text = "│" },
-			change = { text = "│" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
+	opts = function()
+		return {
+			signs = {
+				add = { text = "┃", hl = "GitSignsAdd", numhl = "GitSignsAddNr" },
+				change = { text = "┃", hl = "GitSignsChange", numhl = "GitSignsChangeNr" },
+				delete = { text = "▁", hl = "GitSignsDelete", numhl = "GitSignsDeleteNr" },
+				topdelete = { text = "▔", hl = "GitSignsDelete", numhl = "GitSignsDeleteNr" },
+				changedelete = { text = "┇", hl = "GitSignsChange", numhl = "GitSignsChangeNr" },
+			},
 
-		-- VS Code–style inline blame
-		current_line_blame = true,
-		current_line_blame_opts = {
-			delay = 300,
-			virt_text_pos = "eol",
-		},
-		current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>",
-	},
+			-- VS Code–style inline blame
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 300,
+				virt_text_pos = "eol",
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>",
+		}
+	end,
 }
